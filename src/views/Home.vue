96d8343bfaf698b3a6c5d034650870f2a3017353
Alice <alicexenawong@gmail.com>
2019-12-11 19:06:36 -0800

    Add create and index actions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,72 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <h1>All Contacts</h1>
+    <p>First Name: <input type="text" v-model="newFirstName"></p>
+    <p>Middle Name: <input type="text" v-model="newMiddleName"></p>
+    <p>Last Name: <input type="text" v-model="newLastName"></p>
+    <p>Email: <input type="text" v-model="newEmail"></p>
+    <p>Phone Number: <input type="text" v-model="newPhoneNumber"></p>
+    <p>Bio: <input type="text" v-model="newBio"></p>
+    <button v-on:click="createContact()">Create Contact</button>
+    <div v-for="contact in contacts">
+      <p>ID: {{contact.id}}</p>
+      <p>First Name: {{contact.first_name}}</p>
+      <p>Middle Name: {{contact.middle_name}}</p>
+      <p>Last Name: {{contact.last_name}}</p>
+      <p>Email: {{contact.email}}</p>
+      <p>Phone Number: {{contact.phone_number}}</p>
+      <p>Bio: {{contact.bio}}</p>
+      <hr>
+    </div>
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      contacts: [],
+      newFirstName: "",
+      newMiddleName: "",
+      newLastName: "",
+      newEmail: "",
+      newPhoneNumber: "",
+      newBio: ""
+    };
+  },
+  created: function() {
+    axios.get("api/contacts").then(response => {
+      this.contacts = response.data;
+    });
+  },
+  methods: {
+    createContact: function() {
+      var params = {
+        first_name: this.newFirstName,
+        middle_name: this.newMiddleName,
+        last_name: this.newLastName,
+        email: this.newEmail,
+        phone_number: this.newPhoneNumber,
+        bio: this.newBio
+      };
+      console.log(params);
+      axios.post("api/contacts", params).then(response => {
+        this.contacts.push(response.data);
+        this.newFirstName = "";
+        this.newMiddleName = "";
+        this.newLastName = "";
+        this.newEmail = "";
+        this.newPhoneNumber = "";
+        this.newBio = "";
+      });
+    }
   }
-}
+};
 </script>
